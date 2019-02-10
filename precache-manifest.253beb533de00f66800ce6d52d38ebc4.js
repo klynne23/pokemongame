0562f3b7d1332794fe986d1726fe7d2fa4b18aee
Kirthi Bisset <kirthi.bisset@yahoo.com>
2019-02-10 17:12:18 -0500

    Updates

---
+++ precache-manifest.253beb533de00f66800ce6d52d38ebc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5767df990baad9309441",
+    "url": "/pokemongame/static/css/main.78bf773a.chunk.css"
+  },
+  {
+    "revision": "5767df990baad9309441",
+    "url": "/pokemongame/static/js/main.5767df99.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/pokemongame/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "5278d3149abb114f269d",
+    "url": "/pokemongame/static/js/runtime~main.5278d314.js"
+  },
+  {
+    "revision": "f82f66176c6bd704103db6c4e493a81b",
+    "url": "/pokemongame/index.html"
+  }
+];
